32385ed9aad455139105c92fe58587183cf7f479
bea312 <mukagasirabob@gmail.com>
2026-02-17 13:45:41 -0800

    q1

---
+++ index.js
@@ -0,0 +1,15 @@
+//q1
+
+  let productCategorie= [ "Books", "Eletronics", "Pen", "Paper","Fruits"];
+  //a
+console.log("firstname",productCategorie[0]);
+
+// print last name using .length
+console.log('lastname:', productCategorie [productCategorie.length-1]   );
+
+console.log('totalnumber:',productCategorie.length);
+
+
+productCategorie[1] = "Groceries";
+console.log( ' updatearray',productCategorie);  
+
