acf40acb4dd90f80580a9932fd53712a8edc5bce
bea312 <mukagasirabob@gmail.com>
2026-02-17 13:47:49 -0800

    q2

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ console.log('totalnumber:',productCategorie.length);
 productCategorie[1] = "Groceries";
 console.log( ' updatearray',productCategorie);  
 
+//q2
+
+let inventory = [10, 20, 30];
+inventory.push(40);
+inventory.pop();
+inventory.unshift(0);
+inventory.shift();
+
+
+inventory.splice(1,1,25,35);
+console.log(inventory );   
+
